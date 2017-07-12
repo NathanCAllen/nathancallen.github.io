6a9dd780e2a8999c77b1e7336fea9c4efa4ef761
Nathan Allen <nathan@allenhub.com>
2017-07-11 20:01:20 -0400

    restructured, added node

---
+++ dev/js/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Header } from 'semantic-ui-react';
+
+ReactDOM.render(
+    <h1>hi</h1>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000,
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './dev/js/index.jsx',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        filename: 'js/bundle.min.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ],
+};
