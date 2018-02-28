ff9d5a2bfb8d26ed3c0d38887dc0b1ac8ae8d109
nathancallen <nathan@allenhub.com>
2018-02-28 05:35:58 -0500

    removed old unity project, started unity directory

--- src/pages/Unity.vue
+++ src/pages/Unity.vue
@@ -1,11 +1,14 @@
 <template>
   <main-layout>
-    
+    <div id="gameContainer" style="width: 960px; height: 600px"></div>
   </main-layout>
 </template>
 
 <script>
   import MainLayout from '../layouts/Main.vue'
+
+  let gameInstance = UnityLoader.instantiate("gameContainer", "../HouseOfDiodarra/Build/Phase0_WebGL.json", {onProgress: UnityProgress});
+  console.log(gameInstance);
 
   export default {
     components: {
